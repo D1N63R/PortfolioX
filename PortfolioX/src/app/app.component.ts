b1e187c60f783eafccedd58492ef21529bb527d4
D1N63R <jwmeidinger99@hotmail.com>
2019-04-27 16:56:21 -0500

    Research started

--- PortfolioX/src/app/app.component.ts
+++ PortfolioX/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MainService } from './main.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,34 +8,5 @@ import { MainService } from './main.service';
 })
 export class AppComponent {
   title = "PortfolioX";
-  stock: object;
-  errors: string[];
-  stocks: { name: string, currentPrice: number, priceCompare: string, priceYesterday: number, volume: number, divident: number }[];
-
-  currentPrice = Number;
-  constructor(private _mainService: MainService) {
-    this.stock = { symbol: '' };
-  }
-
-  getCurrentPrice() {
-    this.errors = [];
-    this.stocks = [];
-    this._mainService.getCurrentPrice(this.stock, (stockSymbol, valid) => {
-      if (valid === true) {
-        this.getPrice(stockSymbol);
-      }
-      else {
-        this.errors.push(stockSymbol);
-        this.stock = { symbol: '' };
-      }
-    })
-  }
-
-  getPrice(stockSymbol) {
-    this._mainService.getPrice(stockSymbol, (Name, CurrentPrice, PriceYesterday, Volume, Divident) => {
-      var retrievedStock = { name: Name, currentPrice: CurrentPrice, priceCompare: (CurrentPrice - PriceYesterday).toFixed(2), priceYesterday: PriceYesterday, volume: Volume, divident: Divident };
-      this.stocks.push(retrievedStock);
-      this.stock = { symbol: '' };
-    });
-  }
+  
 }

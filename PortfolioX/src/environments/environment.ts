4d6aa9b05d069dcaf8341d751a2b9bced3abb016
D1N63R <45579118+D1N63R@users.noreply.github.com>
2019-04-28 14:18:40 -0500

    Git doesnt like the API on here

--- PortfolioX/src/environments/environment.ts
+++ PortfolioX/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "AIzaSyAuL9fE1XjPan8oG4hqsKFr9zk1EYLNPkI",
+    apiKey: "",
     authDomain: "practice-ad82e.firebaseapp.com",
     databaseURL: "https://practice-ad82e.firebaseio.com",
     projectId: "practice-ad82e",

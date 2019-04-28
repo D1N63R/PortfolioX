1c3aabb8cbcce823f0e9453e25b1af9d227d088e
D1N63R <jwmeidinger99@hotmail.com>
2019-04-28 14:09:56 -0500

    DB is working now and site now displays the two working components

--- PortfolioX/src/app/app.module.ts
+++ PortfolioX/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MainService } from './main.service';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
 import { ResearchComponent } from './research/research.component';
@@ -10,6 +13,9 @@ import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { ARtickerComponent } from './articker/articker.component';
+import { environment } from 'src/environments/environment';
+import { AddPortComponent } from './add-port/add-port.component';
+
 
 
 @NgModule({
@@ -19,12 +25,17 @@ import { ARtickerComponent } from './articker/articker.component';
     LoginComponent,
     NavBarComponent,
     HomeComponent,
-    ARtickerComponent
+    ARtickerComponent,
+    AddPortComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule],
+    HttpModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireStorageModule
+  ],
   providers: [MainService],
   bootstrap: [AppComponent]
 })
---
+++ PortfolioX/src/app/fire-db.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import { Observable} from 'rxjs';
+import { Portfolio } from './portfolio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireDBService {
+  
+  portfolios: Observable<Portfolio[]>;
+  portfolioCollection : AngularFirestoreCollection;
+
+  portfolioDoc: AngularFirestoreDocument<Portfolio>;
+      
+  constructor(public afs: AngularFirestore){
+      this.portfolios = afs.collection('employees').valueChanges();
+      this.portfolioCollection = this.afs.collection('portfolios');
+  }
+
+  getPortfolios():Observable<Portfolio[]>{
+          
+      console.log(this.portfolios);
+      return this.portfolios;
+
+  }
+
+  addPortfolio(port: Portfolio){
+      this.portfolioCollection.add(port) ;
+  }
+}
